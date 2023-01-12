c84bb0727275d33356fb8d94624564d8c2747751
Dario-Miccinelli <poduzzo@hotmail.com>
2023-01-12 22:25:05 +0100

    prove

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -18,28 +18,33 @@
 
 @section('mainpage-main')
 
-<section class="comicspage">
-<h2>{{$comicsbook['title']}}</h2>
+<section class="comicspage m-auto d-flex flex-wrap m-auto">
+<h2 class="mt-5  ms-5">{{$comicsbook['title']}}</h2>
 
 
 
-<div class="boxes-green">
+<div class="boxes-green d-flex flex-row ms-5">
 
 <div class="green">U.S PRICE {{$comicsbook['price']}} AVAIBLE</div>
 <div class="green">Check Availability</div>
 
 </div>
 
-<p class="paragraph">
+<p class="paragraph  ms-5 me-5">
     {{$comicsbook['description']}}
+
+   
 </p>
-
-
-
-<div id="main-right-side">ADVERTISEMENT
- 
+<div class="ms-5">
+    <div class="text-right">ADVERTISEMENT</div>
+    <img src="{{ asset('images/comics.jpg') }}" alt="" class="ms-5 pb-2">
 </div>
 </section>
+
+
+
+
+
 
 
 
